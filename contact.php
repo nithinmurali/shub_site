4b6200e6ab11ebb0326710cdec5e2d985b6384d5
nithinmurali <imnmfotmal@gmail.com>
2014-09-01 08:47:39 +0530

    added frontend for contact

---
+++ contact.php
@@ -0,0 +1,249 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Untitled</title>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+<meta name="generator" content="Web Page Maker">
+<style type="text/css">
+/*----------Text Styles----------*/
+.ws6 {font-size: 8px;}
+.ws7 {font-size: 9.3px;}
+.ws8 {font-size: 11px;}
+.ws9 {font-size: 12px;}
+.ws10 {font-size: 13px;}
+.ws11 {font-size: 15px;}
+.ws12 {font-size: 16px;}
+.ws14 {font-size: 19px;}
+.ws16 {font-size: 21px;}
+.ws18 {font-size: 24px;}
+.ws20 {font-size: 27px;}
+.ws22 {font-size: 29px;}
+.ws24 {font-size: 32px;}
+.ws26 {font-size: 35px;}
+.ws28 {font-size: 37px;}
+.ws36 {font-size: 48px;}
+.ws48 {font-size: 64px;}
+.ws72 {font-size: 96px;}
+.wpmd {font-size: 13px;font-family: Arial,Helvetica,Sans-Serif;font-style: normal;font-weight: normal;}
+/*----------Para Styles----------*/
+DIV,UL,OL /* Left */
+{
+ margin-top: 0px;
+ margin-bottom: 0px;
+}
+</style>
+
+<script language="JavaScript1.4" type="text/javascript">
+<!--
+function jsPlay(soundobj) {
+ var thissound= eval("document."+soundobj);
+ try {
+     thissound.Play();
+ }
+ catch (e) {
+     thissound.DoPlay();
+ }
+}
+//-->
+</script>
+
+<script language="javascript" type="text/javascript">
+<!--
+function MM_swapImgRestore() {
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() {
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) {
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() {
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+
+//-->
+</script>
+
+</head>
+<body background="images/design gradient floral black background floral texture_wallpaperswa.com_98.jpg" bgProperties="fixed" style="background-attachment:fixed;">
+<div id="shape1" style="position:absolute; overflow:hidden; left:0px; top:11px; width:1330px; height:5px; z-index:0"><img border=0 width="100%" height="100%" alt="" src="images/shape487036875.gif"></div>
+
+<div id="image1" style="position:absolute; overflow:hidden; left:178px; top:12px; width:426px; height:73px; z-index:1"><img src="images/41d000_4cb378833b7ca607a01d7db69d625ff9.png_srz_p_348_60_75_22_0.50_1.20_0.png" alt="" title="" border=0 width=426 height=73></div>
+
+<div id="text1" style="position:absolute; overflow:hidden; left:241px; top:25px; width:410px; height:78px; z-index:2">
+<a href="home.html"><div class="wpmd">
+<div><font color="#CCFFFF" face="Bauhaus 93" class="ws22"><B>SHUBHANKIT</B></font><font face="Bauhaus 93" class="ws22"><B> </B></font><font color="#FF9900" face="Bauhaus 93" class="ws22"><B>RATHORE</B></font></div>
+</div></a></div>
+
+<div id="shape3" style="position:absolute; overflow:hidden; left:255px; top:889px; width:774px; height:53px; z-index:3"><img border=0 width="100%" height="100%" alt="" src="images/shape487036859.gif"></div>
+
+<div id="roundrect5" style="position:absolute; overflow:hidden; left:201px; top:210px; width:917px; height:638px; z-index:4"><img border=0 width="100%" height="100%" alt="" src="images/roundrect487036843.gif"></div>
+
+<div id="image2" style="position:absolute; overflow:hidden; left:200px; top:166px; width:299px; height:44px; z-index:5"><img src="images/41d000_bbde5df8bc11631783b02ab7740eeaf7.png_srz_p_299_44_75_22_0.50_1.20_0.png" alt="" title="" border=0 width=299 height=44></div>
+
+<div id="text6" style="position:absolute; overflow:hidden; left:224px; top:174px; width:125px; height:36px; z-index:6">
+<div class="wpmd">
+<div><font color="#00FFFF" face="Georgia" class="ws20"><B>Contact</B></font></div>
+</div></div>
+
+<div id="text8" style="position:absolute; overflow:hidden; left:224px; top:344px; width:150px; height:90px; z-index:7">
+</div>
+
+<div id="text13" style="position:absolute; overflow:hidden; left:229px; top:324px; width:422px; height:253px; z-index:8">
+<div class="wpmd">
+<div><font face="Harlow Solid Italic" class="ws20"><BR></font></div>
+</div></div>
+
+<div id="shape2" style="position:absolute; overflow:hidden; left:223px; top:211px; width:252px; height:5px; z-index:9"><img border=0 width="100%" height="100%" alt="" src="images/shape487036812.gif"></div>
+
+<div id="html2" style="position:absolute; overflow:hidden; left:520px; top:160px; width:240px; height:42px; z-index:10">
+<div class="fb-like" data-href="https://www.facebook.com/Shubhankit.Rathore" data-width="200" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div></div>
+
+<div id="text7" style="position:absolute; overflow:hidden; left:230px; top:256px; width:738px; height:90px; z-index:11">
+<div class="wpmd">
+<div><font color="#CCFFFF" face="KaiTi" class="ws18">JUST ENTER YOUR DETAILS AND SEND YOUR MESSAGE......</font></div>
+</div></div>
+
+<form id="contact_form" >
+<input name="name" required type="text" style="position:absolute;width:575px;left:350px;top:388px;z-index:12">
+<input name="email" required type="email" style="position:absolute;width:575px;left:350px;top:426px;z-index:13">
+<input name="subject" required id='submit_btn' type="text" style="position:absolute;width:575px;left:350px;top:462px;z-index:14">
+<textarea name="message" required style="position:absolute;left:350px;top:504px;width:575px;height:125px;z-index:15"></textarea>
+<div id="text14" style="position:absolute; overflow:hidden; left:258px; top:384px; width:90px; height:37px; z-index:16">
+<div class="wpmd">
+<div><font color="#CCFFFF" face="Play" class="ws18">Name</font></div>
+</div></div>
+
+<div id="text15" style="position:absolute; overflow:hidden; left:255px; top:425px; width:90px; height:37px; z-index:17">
+<div class="wpmd">
+<div><font color="#CCFFFF" face="Play" class="ws18">E-Mail</font></div>
+</div></div>
+
+<div id="text16" style="position:absolute; overflow:hidden; left:255px; top:460px; width:90px; height:37px; z-index:18">
+<div class="wpmd">
+<div><font color="#CCFFFF" face="Play" class="ws18">Subject</font></div>
+</div></div>
+
+<div id="text17" style="position:absolute; overflow:hidden; left:251px; top:542px; width:103px; height:37px; z-index:19">
+<div class="wpmd">
+<div><font color="#CCFFFF" face="Play" class="ws18">Message</font></div>
+</div></div>
+
+<input name="submit" type="submit" style="position:absolute;left:806px;top:667px;z-index:20">
+</form>
+
+<div id="html1" style="position:absolute; overflow:hidden; left:49px; top:64px; width:200px; height:125px; z-index:21">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script></div>
+
+<div id="nav10d" style="position:absolute; left:279px; top:76px; z-index:22"><a onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('nav10','','images/nav4897886250a.gif',1)" href="home.html"><img name="nav10" onLoad="MM_preloadImages('images/nav4897886250a.gif')" alt="" border=0 src="images/nav4897886250i.gif"></a></div>
+<div id="nav11d" style="position:absolute; left:464px; top:76px; z-index:22"><a onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('nav11','','images/nav4897886251a.gif',1)" href="music.html"><img name="nav11" onLoad="MM_preloadImages('images/nav4897886251a.gif')" alt="" border=0 src="images/nav4897886251i.gif"></a></div>
+<div id="nav12d" style="position:absolute; left:649px; top:76px; z-index:22"><a onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('nav12','','images/nav4897886252a.gif',1)" href="photos.html"><img name="nav12" onLoad="MM_preloadImages('images/nav4897886252a.gif')" alt="" border=0 src="images/nav4897886252i.gif"></a></div>
+<div id="nav13d" style="position:absolute; left:834px; top:76px; z-index:22"><a onMouseOut="MM_swapImgRestore()" onMouseOver="MM_swapImage('nav13','','images/nav4897886253a.gif',1)" href="contact.html"><img name="nav13" onLoad="MM_preloadImages('images/nav4897886253a.gif')" alt="" border=0 src="images/nav4897886253i.gif"></a></div>
+
+<div id="text11" style="position:absolute; overflow:hidden; left:781px; top:900px; width:111px; height:35px; z-index:23">
+<div class="wpmd">
+<div><font color="#00FFFF" face="Play" class="ws16"><B>Facebook</B></font></div>
+</div></div>
+
+<div id="image3" style="position:absolute; overflow:hidden; left:740px; top:899px; width:30px; height:30px; z-index:24"><a href="https://www.facebook.com/Shubhankit.Rathore"><img src="images/b1cd13f9d4dfb1450bbb325285106177.png_srz_p_30_30_85_22_0.50_1.20_0.png" alt="" title="" border=0 width=30 height=30></a></div>
+
+<div id="text12" style="position:absolute; overflow:hidden; left:887px; top:903px; width:95px; height:24px; z-index:25">
+<a href="https://www.facebook.com/Shubhankit.Rathore"><div class="wpmd">
+<div><font color="#00FFFF" face="Shonar Bangla" class="ws14"><B>Become a fan</B></font></div>
+</div></a></div>
+
+<div id="image6" style="position:absolute; overflow:hidden; left:990px; top:906px; width:21px; height:20px; z-index:26"><a href="https://www.facebook.com/Shubhankit.Rathore"><img src="images/41d000_f486b7af7e4bc448eb2ec5a59d8afecc.png_srz_p_21_20_75_22_0.50_1.20_0.png" alt="" title="" border=0 width=21 height=20></a></div>
+
+<div id="text9" style="position:absolute; overflow:hidden; left:310px; top:901px; width:111px; height:35px; z-index:27">
+<div class="wpmd">
+<div><font color="#00FFFF" face="Play" class="ws16"><B>Twitter</B></font></div>
+</div></div>
+
+<div id="image4" style="position:absolute; overflow:hidden; left:269px; top:900px; width:30px; height:30px; z-index:28"><a href="https://twitter.com/ShubhankitR"><img src="images/01113281ebb7dfb57a8dc2a02eb1cb92.png_srz_p_30_30_85_22_0.50_1.20_0.png" alt="" title="" border=0 width=30 height=30></a></div>
+
+<div id="text10" style="position:absolute; overflow:hidden; left:398px; top:904px; width:95px; height:24px; z-index:29">
+<a href="https://twitter.com/ShubhankitR"><div class="wpmd">
+<div><font color="#00FFFF" face="Shonar Bangla" class="ws14"><B>Follow me</B></font></div>
+</div></a></div>
+
+<div id="image5" style="position:absolute; overflow:hidden; left:478px; top:906px; width:21px; height:20px; z-index:30"><a href="https://twitter.com/ShubhankitR"><img src="images/41d000_f486b7af7e4bc448eb2ec5a59d8afecc.png_srz_p_21_20_75_22_0.50_1.20_0.png" alt="" title="" border=0 width=21 height=20></a></div>
+
+<!-- include Google hosted jQuery Library -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<!-- Start jQuery code -->
+<script type="text/javascript">
+$(document).ready(function() {
+    $("#submit_btn").click(function() { 
+       
+        var proceed = true;
+        //simple validation at client's end
+        //loop through each field and we simply change border color to red for invalid fields       
+        $("#contact_form input[required=true], #contact_form textarea[required=true]").each(function(){
+            $(this).css('border-color',''); 
+            if(!$.trim($(this).val())){ //if this field is empty 
+                $(this).css('border-color','red'); //change border color to red   
+                proceed = false; //set do not proceed flag
+            }
+            //check invalid email
+            var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
+            if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
+                $(this).css('border-color','red'); //change border color to red   
+                proceed = false; //set do not proceed flag              
+            }   
+        });
+       
+        if(proceed) //everything looks good! proceed...
+        {
+            //get input field values data to be sent to server
+            post_data = {
+                'user_name'     : $('input[name=name]').val(), 
+                'user_email'    : $('input[name=email]').val(), 
+                'subject'       : $('select[name=subject]').val(), 
+                'msg'           : $('textarea[name=message]').val()
+            };
+            
+            //Ajax post data to server
+            $.post('contact_me.php', post_data, function(response){  
+                if(response.type == 'error'){ //load json data from server and output message     
+                    output = '<div class="error">'+response.text+'</div>';
+                }else{
+                    output = '<div class="success">'+response.text+'</div>';
+                    //reset values in all input fields
+                    $("#contact_form  input[required=true], #contact_form textarea[required=true]").val(''); 
+                    $("#contact_form #contact_body").slideUp(); //hide form after success
+                }
+                $("#contact_form #contact_results").hide().html(output).slideDown();
+            }, 'json');
+        }
+    });
+    
+    //reset previously set border colors and hide all message on .keyup()
+    $("#contact_form  input[required=true], #contact_form textarea[required=true]").keyup(function() { 
+        $(this).css('border-color',''); 
+        $("#result").slideUp();
+    });
+});
+</script>
+
+
+</body>
+</html>
