64527d05ecfc5613de1028d5781cba6796f1222d
nithinmurali <imnmfotmal@gmail.com>
2014-09-03 03:03:29 +0530

    added ajax contact

--- contact.php
+++ contact.php
@@ -115,7 +115,8 @@ function MM_swapImage() {
 <div><font color="#CCFFFF" face="KaiTi" class="ws18">JUST ENTER YOUR DETAILS AND SEND YOUR MESSAGE......</font></div>
 </div></div>
 
-<form id="ajax-contact" method="post" action="mailer.php">
+<div id="form-messages">
+<form id="ajax-contact" method="post" >
 <input type="text" id="name" name="name" required style="position:absolute;width:575px;left:350px;top:388px;z-index:12">
 <input type="email" id="email" name="email" required style="position:absolute;width:575px;left:350px;top:426px;z-index:13">
 <input name="subject" required id='subject' type="text" style="position:absolute;width:575px;left:350px;top:462px;z-index:14">
@@ -142,6 +143,7 @@ function MM_swapImage() {
 
 <input name="submit" type="submit" style="position:absolute;left:806px;top:667px;z-index:20">
 </form>
+</div>
 
 <div id="html1" style="position:absolute; overflow:hidden; left:49px; top:64px; width:200px; height:125px; z-index:21">
 <div id="fb-root"></div>
@@ -188,62 +190,66 @@ function MM_swapImage() {
 
 <!-- include Google hosted jQuery Library -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script>
+  
+$(function() {
 
-<!-- Start jQuery code -->
-<script type="text/javascript">
-$(document).ready(function() {
-    $("#submit_btn").click(function() { 
-       
-        var proceed = true;
-        //simple validation at client's end
-        //loop through each field and we simply change border color to red for invalid fields       
-        $("#contact_form input[required=true], #contact_form textarea[required=true]").each(function(){
-            $(this).css('border-color',''); 
-            if(!$.trim($(this).val())){ //if this field is empty 
-                $(this).css('border-color','red'); //change border color to red   
-                proceed = false; //set do not proceed flag
-            }
-            //check invalid email
-            var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
-            if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
-                $(this).css('border-color','red'); //change border color to red   
-                proceed = false; //set do not proceed flag              
-            }   
-        });
-       
-        if(proceed) //everything looks good! proceed...
-        {
-            //get input field values data to be sent to server
-            post_data = {
-                'user_name'     : $('input[name=name]').val(), 
-                'user_email'    : $('input[name=email]').val(), 
-                'subject'       : $('select[name=subject]').val(), 
-                'msg'           : $('textarea[name=message]').val()
-            };
-            
-            //Ajax post data to server
-            $.post('contact_me.php', post_data, function(response){  
-                if(response.type == 'error'){ //load json data from server and output message     
-                    output = '<div class="error">'+response.text+'</div>';
-                }else{
-                    output = '<div class="success">'+response.text+'</div>';
-                    //reset values in all input fields
-                    $("#contact_form  input[required=true], #contact_form textarea[required=true]").val(''); 
-                    $("#contact_form #contact_body").slideUp(); //hide form after success
-                }
-                $("#contact_form #contact_results").hide().html(output).slideDown();
-            }, 'json');
-        }
+  // Get the form.
+  var form = $('#ajax-contact');
+
+  // Get the messages div.
+  //var formMessages = $('#form-messages');
+
+  // Set up an event listener for the contact form.
+  $(form).submit(function(e) {
+    // Stop the browser from submitting the form.
+    e.preventDefault();
+
+    // Serialize the form data.
+    var formData = $(form).serialize();
+
+    // Submit the form using AJAX.
+    $.ajax({
+      type: 'POST',
+      url: "mailer.php",
+      data: formData
+    })
+    .done(function(response) {
+      
+      // Make sure that the formMessages div has the 'success' class.
+      //$(formMessages).removeClass('error');
+      //$(formMessages).addClass('success');
+      
+      // Set the message text.
+      alert(response);
+
+      // Clear the form.
+      $('#name').val('');
+      $('#email').val('');
+      $('#subject').val('');
+      $('#message').val('');
+    })
+    .fail(function(data) {
+      // Make sure that the formMessages div has the 'error' class.
+      //$(formMessages).removeClass('success');
+      //$(formMessages).addClass('error');
+      //console.log(data.responseText);
+      // Set the message text.
+      if (data.responseText !== '') {
+        //$(formMessages).text(data.responseText);
+        alert(data.responseText);
+      } else {
+        alert('Oops! An error occured and your message could not be sent.');
+        //$(formMessages).text('Oops! An error occured and your message could not be sent.');
+      }
     });
-    
-    //reset previously set border colors and hide all message on .keyup()
-    $("#contact_form  input[required=true], #contact_form textarea[required=true]").keyup(function() { 
-        $(this).css('border-color',''); 
-        $("#result").slideUp();
-    });
+
+  });
+
 });
-</script>
 
+
+</script>
 
 </body>
 </html>
